73931a581e97cde8eb6204735028fcbcfd3094cf
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-12-09 16:00:25 +0100

    Update rag_chat.py

--- RAGChat/rag_chat.py
+++ RAGChat/rag_chat.py
@@ -1,9 +1,9 @@
 import os
-from langchain_postgres import PostgresVectorStore as PGVector
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_ollama import OllamaEmbeddings, ChatOllama
+from langchain_postgres import PGVector
 
 # --- Configuration ---
 # Ensure Ollama is running.
